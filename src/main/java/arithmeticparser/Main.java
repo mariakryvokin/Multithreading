e6511ba377ae1004258a44d1dfb5eea5e80b5aef
Mariia Kryvokin <kruvokin@gmail.com>
2020-04-09 21:31:31 +0300

    added exceptionally to completableFuture

--- src/main/java/arithmeticparser/Main.java
+++ src/main/java/arithmeticparser/Main.java
@@ -3,6 +3,7 @@ package arithmeticparser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -23,33 +24,43 @@ public class Main {
         calculate(parser, expressionInPolishNotation, parametersAndValues);
     }
 
-    private static void calculate(PrimitiveParser parser, String expressionInPolishNotation, CompletableFuture<Map<String, Integer>>[] parametersAndValues) {
-        CompletableFuture.allOf(parametersAndValues).thenRunAsync(()-> {
+    private static void calculate(PrimitiveParser parser, String expressionInPolishNotation,
+                                  CompletableFuture<Map<String, Integer>>[] parametersAndValues) {
+        logger.info("I so close to calculating");
+        CompletableFuture.allOf(parametersAndValues).thenRunAsync(() -> {
             logger.info("I am calculating");
             Calculator calculator = new Calculator();
             try {
-                logger.info(calculator.calculate( replaceParametersWithValues(parser, expressionInPolishNotation, parametersAndValues)));
+                logger.info(calculator.calculate(replaceParametersWithValues(parser, expressionInPolishNotation,
+                        parametersAndValues)));
             } catch (InterruptedException | ExecutionException e) {
-               logger.error(e.getMessage(),e);
+                logger.error(e.getMessage(), e);
             }
             parser.closeResources();
             executorService.shutdown();
-        },executorService);
+        }, executorService);
     }
 
     private static CompletableFuture<Map<String, Integer>>[] getParametersFromClient(PrimitiveParser parser) {
-        System.out.println("Please enter parameters for expression " + expression);
+        logger.info("Please enter parameters for expression " + expression);
         CompletableFuture<Map<String, Integer>>[] allParameters = new CompletableFuture[parser.getParameters().size()];
         for (int i = 0; i < parser.getParameters().size(); i++) {
-            allParameters[i] = parser.specifyParameter(parser.getParameters().get(i));
+            logger.info(i +" parameter: ");
+            String parameterName = parser.getParameters().get(i);
+            allParameters[i] = parser.specifyParameter(parameterName)
+                    .exceptionally(e->{logger.error(e.getMessage());
+                    return Collections.singletonMap(parameterName, 1);});
         }
         return allParameters;
     }
 
-    private static String replaceParametersWithValues(PrimitiveParser parser, String expressionInPolishNotation, CompletableFuture<Map<String, Integer>>[] allParameters) throws InterruptedException, ExecutionException {
+    private static String replaceParametersWithValues(PrimitiveParser parser, String expressionInPolishNotation,
+                                                      CompletableFuture<Map<String, Integer>>[] allParameters)
+            throws InterruptedException, ExecutionException {
         for (int i = 0; i < parser.getParameters().size(); i++) {
             String parameterName = parser.getParameters().get(i);
-            expressionInPolishNotation=  expressionInPolishNotation.replace(parameterName, String.valueOf(allParameters[i].get().get(parameterName)));
+            expressionInPolishNotation = expressionInPolishNotation.replace(parameterName,
+                    String.valueOf(allParameters[i].get().get(parameterName)));
         }
         return expressionInPolishNotation;
     }
